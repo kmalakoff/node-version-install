7b53d54c3de7d843ed8cb6e30e8cc6591c86ca62
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 23:22:39 -0700

    Upgrade modules

--- test/unit/callback.test.ts
+++ test/unit/callback.test.ts
@@ -28,7 +28,7 @@ import spawn from 'cross-spawn-cb';
 // @ts-ignore
 import install from 'node-version-install';
 import { spawnOptions } from 'node-version-utils';
-import validate from '../lib/validate';
+import validate from '../lib/validate.ts';
 
 function addTests(version) {
   describe(version, () => {
