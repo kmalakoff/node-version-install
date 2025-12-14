96aaf9d821c093e610948badd51e017adba045cb
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-14 15:15:21 -0800

    Upgrade modules

--- src/compat.ts
+++ src/compat.ts
@@ -5,11 +5,8 @@
 import os from 'os';
 
 const hasHomedir = typeof os.homedir === 'function';
-
 export function homedir(): string {
-  if (hasHomedir) {
-    return os.homedir();
-  }
+  if (hasHomedir) return os.homedir();
   const home = require('homedir-polyfill');
   return home();
 }
