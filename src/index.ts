15a90bce7c6c7d8242838c9e800e3dcb87015dd4
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-20 14:54:02 -0800

    Migrated to void

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,19 @@ import worker from './workers/install.ts';
 import workerSync from './workers/installSync.ts';
 
 export type * from './types.ts';
-export default function install(versionExpression: string, options?: InstallOptions, callback?: InstallCallback): undefined | Promise<InstallResult[]> {
-  if (typeof options === 'function') {
-    callback = options as InstallCallback;
-    options = {};
-  }
-  options = options || {};
 
-  if (typeof callback === 'function') return worker(versionExpression, options, callback) as undefined;
+export default function install(versionExpression: string): Promise<InstallResult[]>;
+export default function install(versionExpression: string, options: InstallOptions): Promise<InstallResult[]>;
+export default function install(versionExpression: string, callback: InstallCallback): void;
+export default function install(versionExpression: string, options: InstallOptions, callback: InstallCallback): void;
+export default function install(versionExpression: string, options?: InstallOptions | InstallCallback, callback?: InstallCallback): void | Promise<InstallResult[]> {
+  callback = typeof options === 'function' ? options : callback;
+  options = typeof options === 'function' ? {} : ((options || {}) as InstallOptions);
+
+  if (typeof callback === 'function') return worker(versionExpression, options, callback);
   return new Promise((resolve, reject) => worker(versionExpression, options, (err, result) => (err ? reject(err) : resolve(result))));
 }
 
 export function sync(versionExpression: string, options?: InstallOptions): InstallResult[] {
-  options = options || {};
-  return workerSync(versionExpression, options);
+  return workerSync(versionExpression, options || {});
 }
